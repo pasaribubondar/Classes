cb98be34daf56fed9fdf0d351ba68f4ebfcc82f6
pasaribubondar <43933044+pasaribubondar@users.noreply.github.com>
2018-12-17 21:10:34 +0700

    Add files via upload

---
+++ electrical_car.py
@@ -0,0 +1,49 @@
+class Car():
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    def __init__(self,make,model,year):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.odometer_reading=0
+
+    def get_descriptive_name(self):
+        long_name=str(self.year)+' '+self.make+' '+self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("this car has "+str(self.odometer_reading)+"MIles on it.")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
